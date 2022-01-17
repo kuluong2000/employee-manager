a7735fecb641627b221dda1455927a83567e44de
sydung22 <72313449+sydung22@users.noreply.github.com>
2022-01-16 20:40:54 -0700

    vuetify

--- manager_prj/src/router/index.js
+++ manager_prj/src/router/index.js
@@ -1,29 +1,60 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
 
-Vue.use(VueRouter)
+/* Layout */
+import Layout from "../components/Layout";
+
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "dashboard",
+    component: Layout,
+    children: [
+      {
+        path: "/",
+        component: () => import("../views/Dashboard"),
+        name: "Dashboard",
+      },
+      {
+        path: "/user",
+        name: "dashboard",
+        component: () => import("../views/User"),
+      },
+      {
+        path: "/employee",
+        name: "dashboard",
+        component: () => import("../views/Employee"),
+      },
+      {
+        path: "/facilities",
+        name: "dashboard",
+        component: () => import("../views/Facilities"),
+      },
+      {
+        path: "/department",
+        name: "dashboard",
+        component: () => import("./../views/Department"),
+      },
+      {
+        path: "/position",
+        name: "dashboard",
+        component: () => import("./../views/Position"),
+      },
+    ],
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    path: "/login",
+    name: "login",
+    component: () => import("./../views/Login"),
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
---
+++ manager_prj/src/views/Position.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>This is an Position page</h1>
+  </div>
+</template>
