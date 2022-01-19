39feac59353863d971470ea7284ea5519e8da976
kuluong2000 <tranhuuluong156200@gmail.com>
2022-01-19 22:43:28 +0700

    update project login

---
+++ .history/manager_prj/src/main_20220118201705.js
@@ -0,0 +1,13 @@
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import router from "./router";
+import axios from "axios";
+
+Vue.config.productionTip = false;
+axios.defaults.baseURL = "http://localhost:3001";
+new Vue({
+  vuetify,
+  router,
+  render: (h) => h(App),
+}).$mount("#app");
